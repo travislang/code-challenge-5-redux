ef23b76ba9e623c3b87f6326af587569dc36951d
Travis Lang <tlang50@icloud.com>
2018-12-10 13:28:43 -0600

    add passenger logic

--- src/components/Passengers/Passengers.js
+++ src/components/Passengers/Passengers.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 // THIS COMPONENT IS OUR INTERFACE FOR PASSENGER CHECK IN
 // YOU SHOULD DISPLAY THE CURRENT PASSENGERS
 // INPUT SHOULD COLLECT INFO, BUTTON SHOULD ADD THEM TO THE LIST
 
 class Passengers extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Passengers</h2>
 
-        <input type="text" name="name" placeholder="Enter Name" />
-        <button>Add Passenger</button>
+    state = {
+        newPassenger: ''
+    }
 
-        <ul>PASSENGER LIST: GOES HERE</ul>
-      
-      </div>
-    )
-  }
+    handleChange = (e) => {
+        this.setState({
+            newPassenger: e.target.value
+        })
+    }
+
+    handleClick = () => {
+        this.props.dispatch({type: 'ADD_PASSENGER', payload: this.state.newPassenger})
+        this.setState({
+            newPassenger: ''
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Passengers</h2>
+
+                <input type="text" name="name" placeholder="Enter Name" value={this.state.newPassenger} onChange={this.handleChange} />
+                <button onClick={this.handleClick}>Add Passenger</button>
+
+                <ul>{this.props.reduxStore.passengers.map( person => {
+                    return <li>{person}</li>
+                })}</ul>
+
+            </div>
+        )
+    }
 }
 
-export default Passengers;
+const mapReduxStateToProps = (reduxStore) => {
+    return {
+        reduxStore
+    }
+}
+
+export default connect(mapReduxStateToProps)(Passengers);
